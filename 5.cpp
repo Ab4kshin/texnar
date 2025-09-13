1b9a1184da09f0ed3310682da148b2c097e87e70
Dmitriy <110955867+Ab4kshin@users.noreply.github.com>
2025-09-14 00:07:39 +0500

    added 5_no_ai

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+  
+int main()
+{   
+    string cplusplus = "Uchi cplusplus\n";
+    for (int i = 0; i < 10; ++i){
+        cout << cplusplus;
+    }
+    return 0;
+}
